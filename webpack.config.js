5df9c077a4078daab12976c0a0e852844e88fb4e
Henry Nawrocki <hdev@sztanski.com>
2022-03-22 08:46:55 -0500

    Modify webpack.config.js rules to fix css processing

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,6 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-    ],
-  },
-  module: {
-    rules: [
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
